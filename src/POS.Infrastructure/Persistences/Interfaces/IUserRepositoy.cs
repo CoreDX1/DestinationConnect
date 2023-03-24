bfa9f0519c8b481d84ff10f39803ed9c250c6af8
CoreDX1 <chrismaquis@gmail.com>
2023-03-24 19:05:48 -0300

    Cambinado los parametros

--- src/POS.Infrastructure/Persistences/Interfaces/IUserRepositoy.cs
+++ src/POS.Infrastructure/Persistences/Interfaces/IUserRepositoy.cs
@@ -5,5 +5,5 @@ namespace POS.Infrastructure.Persistences.Interfaces;
 public interface IUserRepositoy
 {
     public Task<bool> AddUser(User user);
-    public Task<User> GetUser(string email, string password);
+    public Task<User> GetUser(User user);
 }
