d273ffb4b87633e0c404dd8de79d758cf8149d55
CoreDX1 <chrismaquis@gmail.com>
2023-03-24 18:58:29 -0300

    Creando el Context

---
+++ src/POS.Infrastructure/Persistences/Context/DestinationConnectContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using POS.Domain.Entities;
+
+namespace POS.Infrastructure.Persistences.Context;
+
+public partial class DestinationConnectContext : DbContext
+{
+    public DestinationConnectContext(DbContextOptions<DestinationConnectContext> options)
+        : base(options) { }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
