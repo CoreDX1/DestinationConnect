4eb159b435a1df76c8c8f2fbe1b8ad2674eadb2c
CoreDX1 <chrismaquis@gmail.com>
2023-03-24 20:30:31 -0300

    Removinedo los Using que no usamos

--- src/POS.Domain/Entities/User.cs
+++ src/POS.Domain/Entities/User.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace POS.Domain.Entities;
+﻿namespace POS.Domain.Entities;
 
 public partial class User
 {
@@ -14,6 +11,4 @@ public partial class User
     public string Email { get; set; } = null!;
 
     public string Password { get; set; } = null!;
-
-    public DateTime? RegistrationDate { get; set; }
 }
