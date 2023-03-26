bfd019788e533e69ff5dd9ca416007213c0e85ef
CoreDX1 <chrismaquis@gmail.com>
2023-03-26 12:09:05 -0300

    Login y Register

---
+++ src/POS.Api/ClientApp/src/routes/auth/Register/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik"
+
+export default component$(() => {
+    return (
+        <>
+            <div>
+                <h1>Hola mundo</h1>
+            </div>
+        </>
+    )
+})
---
+++ src/POS.Api/ClientApp/src/routes/auth/login/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik"
+
+export default component$(() => {
+    return (
+        <>
+            <div class="bg-black">
+                <h1>Registro</h1>
+            </div>
+        </>
+    )
+})
