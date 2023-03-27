b688c30055adf79bd7626ed3261711a600cae969
CoreDX1 <chrismaquis@gmail.com>
2023-03-27 16:49:34 -0300

    Agregando Un POST para el login

--- src/POS.Api/ClientApp/src/routes/auth/login/index.tsx
+++ src/POS.Api/ClientApp/src/routes/auth/login/index.tsx
@@ -1,10 +1,61 @@
-import { component$ } from "@builder.io/qwik"
+import { $, component$, type QwikChangeEvent, useStore } from "@builder.io/qwik"
+import { type ResponseLogin, type Login } from "~/Interface/IUser"
+import axios from "axios"
 
 export default component$(() => {
+    const loginUser = useStore<Login>({
+        email: "",
+        password: "",
+    })
+
+    const onChangePasswod = $((e: QwikChangeEvent<HTMLInputElement>): void => {
+        const { value } = e.target
+        loginUser.password = value
+    })
+
+    const onChangeEmail = $((e: QwikChangeEvent<HTMLInputElement>): void => {
+        const { value } = e.target
+        loginUser.email = value
+    })
+
+    const webLogin = $(async () => {
+        const res = await axios<ResponseLogin>(
+            "http://localhost:5278/api/User/Auth/Login",
+            {
+                method: "post",
+                data: loginUser,
+            }
+        )
+        console.log(res.data)
+    })
+
     return (
         <>
-            <div class="bg-black">
-                <h1>Registro</h1>
+            <div>
+                <form>
+                    <h1>Please sign in</h1>
+                    <div>
+                        <div>
+                            <input
+                                type="email"
+                                placeholder="Enter Email"
+                                onChange$={onChangeEmail}
+                            />
+                            <input
+                                type="password"
+                                placeholder="Enter Password"
+                                onChange$={onChangePasswod}
+                            />
+                            <button
+                                class="bg-fuchsia-50"
+                                preventdefault:click
+                                onClick$={webLogin}
+                            >
+                                Login
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </>
     )
