1f36cf3e38349d4906bf5bed468276139ff989a8
CoreDX1 <chrismaquis@gmail.com>
2023-03-27 20:34:22 -0300

    El Menu de la pagina

---
+++ src/POS.Api/ClientApp/src/components/header/header.tsx
@@ -0,0 +1,77 @@
+import { component$ } from "@builder.io/qwik"
+import facebook from "../../../public/Logo/facebook-logo-meta.svg"
+import youtube from "../../../public/Logo/youtube.svg"
+import instagram from "../../../public/Logo/instagram.svg"
+import arrow from "../../../public/Logo/arrow-down.svg"
+
+const icon = [
+    {
+        src: facebook,
+    },
+    {
+        src: youtube,
+    },
+    {
+        src: instagram,
+    },
+]
+const listMenu = [
+    {
+        name: "Destinations",
+        href: "/home",
+    },
+    {
+        name: "Travel Tips",
+        href: "/travel-tips",
+    },
+    {
+        name: "Planning",
+        href: "/planning",
+    },
+    {
+        name: "About",
+        href: "/about",
+    },
+    {
+        name: "Shop",
+        href: "/shop",
+    },
+]
+
+export default component$(() => {
+    return (
+        <header>
+            <nav class="flex flex-row justify-center items-center gap-16">
+                    <a class="text-[30px]" href="/home">Titulo</a>
+                    <ul>
+                        {listMenu.map((item, index) => (
+                            <li key={index} class="inline-block px-5">
+                                <a class="pr-1.5" href={item.href}>
+                                    {item.name}
+                                </a>
+                                <img
+                                    src={arrow}
+                                    class="inline-block"
+                                    alt="arrow-down"
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                <div>
+                    <ul>
+                        {icon.map((item, index) => (
+                            <img
+                                key={index}
+                                src={item.src}
+                                class="inline-block w-5"
+                                alt="arrow-down"
+                            />
+                        ))}
+                        <a href="/auth/login">Login</a>
+                        <a href="/auth/register">Register</a>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
+})
