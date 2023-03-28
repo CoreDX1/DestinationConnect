53b06a0b0af17ad713a3283cf7b6d8bc09ee8d50
CoreDX1 <chrismaquis@gmail.com>
2023-03-28 19:35:44 -0300

    Ignorando ErroCount

--- src/POS.Application/DTO/Response/ErrorResponseDto.cs
+++ src/POS.Application/DTO/Response/ErrorResponseDto.cs
@@ -1,7 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace POS.Application.DTO.Response;
 
 public class ErrorResponseDto
 {
     public List<string>? Email { get; set; }
     public List<string>? Password { get; set; }
+
+    [JsonIgnore]
+    public int ErrorCount
+    {
+        get { return Email!.Count() + Password!.Count(); }
+    }
 }
