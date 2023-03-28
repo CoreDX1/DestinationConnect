39fc58b66555b8fbc3c777e3656e5d87a1973d1f
CoreDX1 <chrismaquis@gmail.com>
2023-03-28 16:48:03 -0300

    Agreagando en las extensiones

--- src/POS.Application/Commons/Base/BaseResponse.cs
+++ src/POS.Application/Commons/Base/BaseResponse.cs
@@ -1,4 +1,4 @@
-using FluentValidation.Results;
+using POS.Application.DTO.Response;
 
 namespace POS.Application.Commons.Base;
 
@@ -7,5 +7,5 @@ public class BaseResponse<T>
     public bool Success { get; set; }
     public T? Data { get; set; }
     public string? Message { get; set; }
-    public IEnumerable<ValidationFailure>? Errors { get; set; }
+    public ErrorResponseDto? Errors { get; set; }
 }
