bd352f0c322f45f84c5bf5e0be76a232b4b79757
CoreDX1 <chrismaquis@gmail.com>
2023-04-04 18:10:07 -0300

    AAA

--- src/POS.Api/ClientApp/src/components/errorForm/index.tsx
+++
@@ -1,6 +0,0 @@
-import { component$ } from "@builder.io/qwik"
-import { type IUserResponse } from "~/Interface/Response/IUserResponse"
-
-export const ErrorForm = component$<IUserResponse>((prop) => {
-    return <p>{prop.message}</p>
-})
