c739b898da9fbc929a98809851f3b708046291ce
CoreDX1 <chrismaquis@gmail.com>
2023-04-04 18:08:52 -0300

    Usando el componente de errores

--- src/POS.Api/ClientApp/src/routes/auth/register/index.tsx
+++ src/POS.Api/ClientApp/src/routes/auth/register/index.tsx
@@ -8,6 +8,7 @@ import {
 import type { IUserResponse } from "~/Interface/Response/IUserResponse"
 import { User } from "~/api/UserApi"
 import { type Register } from "~/Interface/Request/IUserRequest"
+import { ErrorList } from "~/components/errorList"
 
 export default component$(() => {
     const registrationData = useStore<Register>({
@@ -50,6 +51,14 @@ export default component$(() => {
                                 <label class="block mb-2 text-sm font-medium text-gray-900">
                                     Your FirstName
                                 </label>
+                                {!resgistrationResponse.value?.success && (
+                                    <ErrorList
+                                        message={
+                                            resgistrationResponse.value?.errors
+                                                .firstName
+                                        }
+                                    />
+                                )}
                                 <input
                                     type="text"
                                     placeholder="firstname"
@@ -66,6 +75,14 @@ export default component$(() => {
                                 >
                                     Your Lastname
                                 </label>
+                                {!resgistrationResponse.value?.success && (
+                                    <ErrorList
+                                        message={
+                                            resgistrationResponse.value?.errors
+                                                .lastName
+                                        }
+                                    />
+                                )}
                                 <input
                                     type="text"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -80,18 +97,12 @@ export default component$(() => {
                                     Your Email
                                 </label>
                                 {!resgistrationResponse.value?.success && (
-                                    <div>
-                                        {resgistrationResponse.value?.errors.email.map(
-                                            (item, index) => (
-                                                <p
-                                                    key={index}
-                                                    class="text-red-600"
-                                                >
-                                                    {item}
-                                                </p>
-                                            )
-                                        )}
-                                    </div>
+                                    <ErrorList
+                                        message={
+                                            resgistrationResponse.value?.errors
+                                                .email
+                                        }
+                                    />
                                 )}
                                 <input
                                     type="email"
@@ -107,18 +118,12 @@ export default component$(() => {
                                     Your Password
                                 </label>
                                 {!resgistrationResponse.value?.success && (
-                                    <div>
-                                        {resgistrationResponse.value?.errors.password.map(
-                                            (item, index) => (
-                                                <p
-                                                    key={index}
-                                                    class="text-red-600"
-                                                >
-                                                    {item}
-                                                </p>
-                                            )
-                                        )}
-                                    </div>
+                                    <ErrorList
+                                        message={
+                                            resgistrationResponse.value?.errors
+                                                .password
+                                        }
+                                    />
                                 )}
                                 <input
                                     type="password"
