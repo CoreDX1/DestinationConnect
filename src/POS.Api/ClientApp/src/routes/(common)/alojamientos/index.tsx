f80b593e95667a474602902fe864e3380ad59ceb
CoreDX1 <chrismaquis@gmail.com>
2023-04-10 19:50:43 -0300

    Llamando al component

--- src/POS.Api/ClientApp/src/routes/(common)/alojamientos/index.tsx
+++ src/POS.Api/ClientApp/src/routes/(common)/alojamientos/index.tsx
@@ -1,10 +1,12 @@
 import { component$ } from "@builder.io/qwik"
+import { Content } from "~/components/content"
 import { Formfilter } from "~/components/formFilter"
 
 export default component$(() => {
     return (
         <>
             <Formfilter />
+            <Content />
         </>
     )
 })
