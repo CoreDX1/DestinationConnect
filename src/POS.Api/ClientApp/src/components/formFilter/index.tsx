5946303094c06fa14d986d0586636368ff31af04
CoreDX1 <chrismaquis@gmail.com>
2023-04-10 16:38:45 -0300

    Creando Los input para el form

--- src/POS.Api/ClientApp/src/components/formFilter/index.tsx
+++ src/POS.Api/ClientApp/src/components/formFilter/index.tsx
@@ -1,4 +1,10 @@
-import { $, component$, type QwikMouseEvent, useStore } from "@builder.io/qwik"
+import {
+    $,
+    component$,
+    useStore,
+    type QwikChangeEvent,
+    type QwikMouseEvent,
+} from "@builder.io/qwik"
 import type {
     WithLabel,
     IFormfilter,
@@ -8,14 +14,10 @@ import menuItem from "~/api/LocalApi/MenuItem.json"
 
 export const Formfilter = component$(() => {
     const form = useStore<IFormfilter>({
-        casa: "",
+        alojamiento: "",
         destino: "",
         fechaInicio: "",
         fechaFin: "",
-        habitaciones: {
-            cama: 0,
-            personas: 0,
-        },
     })
 
     const createItemsWithId = <T extends WithLabel>(
@@ -28,53 +30,74 @@ export const Formfilter = component$(() => {
     }
     const newMenu = createItemsWithId(menuItem)
 
+    const handleTest = $((e: QwikChangeEvent<HTMLInputElement>) => {
+        const { value, name } = e.target
+        form[name] = value
+        console.log(form)
+    })
+
     const handleSubmit = $(
         (event: QwikMouseEvent<HTMLSpanElement, MouseEvent>) => {
             const { id } = event.target as HTMLSpanElement
-            form.casa = id
+            const textModicate = id.replace(/\s+/g, "-").toLowerCase()
+            form["alojamiento"] = textModicate
+            console.log(form)
         }
     )
 
     return (
-        <div class="flex justify-center items-center py-5 bg-violet-400">
-            <div class="bg-indigo-900 w-[60rem] rounded-md p-4">
+        <div class="flex justify-center items-center py-5 bg-gradient-to-b from-purple-400 to-purple-500">
+            <div class="bg-[#270570] w-[60rem] rounded-md p-4">
                 <div class="text-white flex flex-row gap-7 pb-6 items-center">
                     <h2 class="text-2xl font-medium">Alojamientos</h2>
                     {newMenu.map((item, index) => (
                         <div key={index}>
-                            <p>
-                                <span
-                                    id={item.id}
-                                    class="border-2 rounded-full px-4"
-                                    onClick$={() => handleSubmit}
-                                >
-                                    {item.label}
-                                </span>
-                            </p>
+                            <span
+                                class="border-2 rounded-full px-4"
+                                id={item.label}
+                                onClick$={handleSubmit}
+                            >
+                                {item.label}
+                            </span>
                         </div>
                     ))}
                 </div>
-                <form class="text-gray-500">
-                    <label>Destino</label>
-                    <input
-                        type="text"
-                        placeholder="ingresa una cuidad , alojamiento o punto de"
-                    />
-                    <div>
-                        <label>Fecha de inicio</label>
-                        <input type="date" placeholder="ingresa una fecha" />
-                        <label>Fecha de fin</label>
-                        <input type="date" placeholder="ingresa una fecha" />
+                <form
+                    class="text-gray-500 flex flex-row gap-3"
+                    preventdefault:submit
+                >
+                    <div class="bg-white p-2">
+                        <label class="block text-sm">Destino</label>
+                        <input
+                            type="text"
+                            name="destino"
+                            placeholder="ingresa una cuidad , alojamiento o punto de"
+                            onChange$={handleTest}
+                        />
                     </div>
-                    <label>Habitaciones</label>
-                    <div>
-                        <h5>cama</h5>
-                        <input type="text" value={form.habitaciones.cama} />
+                    <div class="flex flex-row ">
+                        <div class="bg-white p-2">
+                            <label class="block">Fecha de inicio</label>
+                            <input
+                                type="date"
+                                name="fechaInicio"
+                                placeholder="ingresa una fecha"
+                                onChange$={handleTest}
+                            />
+                        </div>
+                        <div class="bg-white p-2">
+                            <label class="block">Fecha de fin</label>
+                            <input
+                                type="date"
+                                name="fechaFin"
+                                placeholder="ingresa una fecha"
+                                onChange$={handleTest}
+                            />
+                        </div>
                     </div>
-                    <div>
-                        <h5>personas</h5>
-                        <input type="text" value={form.habitaciones.personas} />
-                    </div>
+                    <button class="bg-orange-500 w-[120px] text-white">
+                        Enviar
+                    </button>
                 </form>
             </div>
         </div>
