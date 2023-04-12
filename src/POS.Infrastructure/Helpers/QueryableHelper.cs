9aa499985fe6587362f7d5667bbc88ac65a75bdf
CoreDX1 <chrismaquis@gmail.com>
2023-04-12 19:40:38 -0300

    Pagination

---
+++ src/POS.Infrastructure/Helpers/QueryableHelper.cs
@@ -0,0 +1,14 @@
+using POS.Infrastructure.Commons.Base.Request;
+
+namespace POS.Infrastructure.Helpers;
+
+public static class QueryableHelpers
+{
+    public static IQueryable<T> Paginate<T>(
+        this IQueryable<T> queryable,
+        BasePaginationRequest request
+    )
+    {
+        return queryable.Skip((request.NumPage - 1) * request.Records).Take(request.Records);
+    }
+}
