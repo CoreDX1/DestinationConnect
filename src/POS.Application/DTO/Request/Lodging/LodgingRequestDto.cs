485ca68cd057043073c0c9e2642e4091da9f2e51
CoreDX1 <chrismaquis@gmail.com>
2023-04-13 15:13:02 -0300

    Creando el modelo de la Request

---
+++ src/POS.Application/DTO/Request/Lodging/LodgingRequestDto.cs
@@ -0,0 +1,8 @@
+namespace POS.Application.DTO.Request.Lodging;
+
+public class LodgingRequestDto
+{
+    public string? Locatily { get; set; }
+    public string? Description { get; set; }
+    public int? State { get; set; }
+}
