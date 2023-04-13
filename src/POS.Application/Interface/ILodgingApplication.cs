ff278558e153538783c83bfaf2d9504bd09a9911
CoreDX1 <chrismaquis@gmail.com>
2023-04-13 15:16:40 -0300

    Creando la interfaces

--- src/POS.Application/Interface/ILodgingApplication.cs
+++ src/POS.Application/Interface/ILodgingApplication.cs
@@ -1,8 +1,15 @@
+using POS.Application.Commons.Base;
+using POS.Application.DTO.Response.Lodging;
 using POS.Domain.Entities;
+using POS.Infrastructure.Commons.Base.Request;
+using POS.Infrastructure.Commons.Base.Response;
 
 namespace POS.Application.Interface;
 
 public interface ILodgingApplication
 {
     Task<IEnumerable<Lodging>> GetLodgings();
+    Task<BaseResponse<BaseEntityResponse<LodgingResponseDto>>> ListLodgings(
+        BaseFiltersRequest filters
+    );
 }
