e1fa7ed8fa8e363be7b51ab7b6f7f84ef01ab593
CoreDX1 <chrismaquis@gmail.com>
2023-04-13 16:04:13 -0300

    Agregando el filtero de Typ

--- src/POS.Application/DTO/Response/Lodging/LodgingResponseDto.cs
+++ src/POS.Application/DTO/Response/Lodging/LodgingResponseDto.cs
@@ -4,6 +4,7 @@ public class LodgingResponseDto
 {
     public int Id { get; set; }
     public string? Locality { get; set; }
+    public string? LodgingType { get; set; }
     public string? Description { get; set; }
     public DateTime DateStart { get; set; }
     public int State { get; set; }
