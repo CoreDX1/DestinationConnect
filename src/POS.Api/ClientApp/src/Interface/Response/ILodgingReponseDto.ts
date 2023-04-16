df4664793788d6c6e6a7b56bca1282d6520c6303
CoreDX1 <chrismaquis@gmail.com>
2023-04-16 16:51:18 -0300

    Respuesto de Lodging

---
+++ src/POS.Api/ClientApp/src/Interface/Response/ILodgingReponseDto.ts
@@ -0,0 +1,9 @@
+export interface ILodgingResponseDto {
+       id: number,
+        locality: string,
+        lodgingType:  string,
+        description: string
+        dateStart:  string
+        dateEnd:    string
+        state: number
+}
