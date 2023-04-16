674841f8c0ede7aaa0afe8afae740a0e9bffb574
CoreDX1 <chrismaquis@gmail.com>
2023-04-16 16:50:29 -0300

    Refactoriznaod

--- src/POS.Api/ClientApp/src/Interface/Response/IUserResponse.ts
+++ src/POS.Api/ClientApp/src/Interface/Response/IUserResponse.ts
@@ -1,11 +1,4 @@
-export interface IUserResponse {
-    success?: boolean  
-    message?: string 
-    data: string
-    errors: Error 
-}
-
-interface Error {
+export interface Error {
     email: string[]
     password: string[]
     firstName : string[]
