abe076da3ee1ba7672a56a8b0fedf882efdf9fb9
CoreDX1 <chrismaquis@gmail.com>
2023-04-17 18:12:28 -0300

    Fetch

--- src/POS.Api/ClientApp/src/service/LodgingApi.ts
+++ src/POS.Api/ClientApp/src/service/LodgingApi.ts
@@ -1,24 +1,28 @@
 import axios from "axios"
 import { type BaseReponse } from "~/Commons/Base/BaseResponse"
-import { type ILodgingRequestDto } from "~/Interface/Request/ILodgingRequestDto"
-import { type ILodgingResponseDto } from "~/Interface/Response/ILodgingReponseDto"
-import { URL_API } from "~/data/constantes"
+// import { type ILodgingRequestDto } from "~/Interface/Request/ILodgingRequestDto"
+import type {
+    Items,
+} from "~/Interface/Response/ILodgingReponseDto"
+import { URL_API, } from "~/data/constantes"
 
 class Lodging {
     public ListLedging = async () => {
-        const { data } = await axios<BaseReponse<ILodgingResponseDto[]>>({
-            url: `${URL_API}/Lodging/select`,
-            method: "GET",
-        })
+        const { data } = await axios.get<BaseReponse<Items[]>>(
+            `${URL_API}/Lodging/select`
+        )
         return data
     }
 
-    public async FilterLedging(filter: ILodgingRequestDto) {
-        const { data } = await axios<BaseReponse<ILodgingResponseDto[]>>({
-            method: "POST",
-            url: `${URL_API}/Lodging`,
-            data: filter,
-        })
+    // public async FilterLedging(filter: ILodgingRequestDto) {
+    //     const { data } = await axios.post<
+    //         BaseReponse<ILodgingReponseDto<Items[]>>
+    //     >(`${URL_API}/Lodging`, filter)
+    //     return data
+    // }
+
+    public async FilterLedging() {
+        const { data } = await axios.get("http://localhost:5278/api/Lodging/filter/numPage=1&numRecordPage=5&numFilters=1&textLodgingType=Hotel&textFilter=Miami&startData=2023-01-01&endData=2023-12-02")
         return data
     }
 }
