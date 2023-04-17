3ec45404834f7e11a24bbcfec4167c94d8e073d0
CoreDX1 <chrismaquis@gmail.com>
2023-04-17 11:45:46 -0300

    AAA

--- src/POS.Api/ClientApp/src/components/successMessage.tsx
+++ src/POS.Api/ClientApp/src/components/successMessage.tsx
@@ -1,21 +1,27 @@
 import { component$ } from "@builder.io/qwik"
-import type { BaseReponse } from "~/Commons/Base/BaseResponse"
 
-export const SuccessMessage = component$(
-    ({
-        message,
-        success,
-    }: Pick<BaseReponse<boolean>, "message" | "success">) => {
+type Props = {
+    message?: string
+    success?: boolean
+    show?: boolean
+}
+
+export const SuccessMessage = component$<Props>(
+    ({ message, success, show }) => {
         return (
-            <p
-                class={
-                    success
-                        ? "text-green-500 text-[40px]"
-                        : "text-red-500 text-[40px]"
-                }
-            >
-                {message}
-            </p>
+            <>
+                {show && (
+                    <p
+                        class={
+                            success
+                                ? "text-green-500 text-[40px]"
+                                : "text-red-500 text-[40px]"
+                        }
+                    >
+                        {message}
+                    </p>
+                )}
+            </>
         )
     }
 )
