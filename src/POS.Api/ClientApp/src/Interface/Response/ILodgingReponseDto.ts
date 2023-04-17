26c3fb17350785aa1ef328a707ff2c5c518e8c9c
CoreDX1 <chrismaquis@gmail.com>
2023-04-17 18:11:11 -0300

    Renombrando

--- src/POS.Api/ClientApp/src/Interface/Response/ILodgingReponseDto.ts
+++ src/POS.Api/ClientApp/src/Interface/Response/ILodgingReponseDto.ts
@@ -1,6 +1,12 @@
-export interface ILodgingResponseDto {
+export interface ILodgingReponseDto<T>{
+        totalRecords : number
+        items : T
+}
+
+export interface Items {
     id: number
     locality: string
+    price: number
     lodgingType: string
     description: string
     dateStart: string
