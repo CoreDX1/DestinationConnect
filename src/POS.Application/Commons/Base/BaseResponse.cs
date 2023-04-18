e1a55db96379a368f77f14a05b993c4de27a0caa
CoreDX1 <chrismaquis@gmail.com>
2023-04-18 10:43:20 -0300

    Inicializando el objeto

--- src/POS.Application/Commons/Base/BaseResponse.cs
+++ src/POS.Application/Commons/Base/BaseResponse.cs
@@ -1,5 +1,3 @@
-using POS.Application.DTO.Response;
-
 namespace POS.Application.Commons.Base;
 
 public class BaseResponse<T>
@@ -7,5 +5,10 @@ public class BaseResponse<T>
     public bool Success { get; set; }
     public T? Data { get; set; }
     public string? Message { get; set; }
-    public ErrorResponseDto? Errors { get; set; }
+    public BaseResponseError? Errors { get; set; }
+
+    public BaseResponse()
+    {
+        Errors = new BaseResponseError();
+    }
 }
