735f943d50895128b1941c568a95ec8171c27400
CoreDX1 <chrismaquis@gmail.com>
2023-04-18 10:44:34 -0300

    Crendo un clase para los erros de request

---
+++ src/POS.Application/ErrorValidation/UserError/MessageError.cs
@@ -0,0 +1,73 @@
+using FluentValidation;
+using POS.Application.DTO.Request.User;
+using POS.Application.DTO.Response.User;
+using POS.Application.Interface;
+using POS.Domain.Entities;
+using POS.Infrastructure.Persistences.Interfaces;
+
+namespace POS.Application.ErrorValidation.UserError;
+
+public class MessageError : IMessageError
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IValidator<UserRequestDto> _validatorLogin;
+    private readonly IValidator<User> _validatorRegister;
+
+    public MessageError(
+        IUnitOfWork unitOfWork,
+        IValidator<UserRequestDto> _valdatorLogin,
+        IValidator<User> validatorRegister
+    )
+    {
+        _unitOfWork = unitOfWork;
+        _validatorLogin = _valdatorLogin;
+        _validatorRegister = validatorRegister;
+    }
+
+    public async Task<ErrorLoginResponseDto> Login(UserRequestDto user)
+    {
+        var validateResult = await _validatorLogin.ValidateAsync(user);
+
+        var errorResponse = new ErrorLoginResponseDto
+        {
+            Email = new List<string>(),
+            Password = new List<string>(),
+        };
+
+        // TODO: When the user needs to login
+        foreach (var error in validateResult.Errors)
+        {
+            List<string>? errorMessageList =
+                errorResponse.GetType().GetProperty(error.PropertyName)?.GetValue(errorResponse)
+                as List<string>;
+            errorMessageList?.Add(error.ErrorMessage);
+        }
+        return errorResponse;
+    }
+
+    public async Task<ErrorRegisterResponseDto> Register(User user)
+    {
+        var isEmailValid = await _unitOfWork.Users.ValidateEmail(user);
+        var errorResponse = new ErrorRegisterResponseDto
+        {
+            Email = new List<string>(),
+            FirstName = new List<string>(),
+            LastName = new List<string>(),
+            Password = new List<string>(),
+        };
+
+        // TODO: When the user needs to register
+        errorResponse.Email.Add("El email ya existe");
+        var validateResult = await _validatorRegister.ValidateAsync(user);
+
+        // TODO: When the user needs to login
+        foreach (var error in validateResult.Errors)
+        {
+            List<string>? errorMessageList =
+                errorResponse.GetType().GetProperty(error.PropertyName)?.GetValue(errorResponse)
+                as List<string>;
+            errorMessageList?.Add(error.ErrorMessage);
+        }
+        return errorResponse;
+    }
+}
