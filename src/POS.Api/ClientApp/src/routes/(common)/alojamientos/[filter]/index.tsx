cd256b081d419144fae8a12fc93f0bfe975d77bc
CoreDX1 <chrismaquis@gmail.com>
2023-04-19 20:09:35 -0300

    Pagination Lista

--- src/POS.Api/ClientApp/src/routes/(common)/alojamientos/[filter]/index.tsx
+++ src/POS.Api/ClientApp/src/routes/(common)/alojamientos/[filter]/index.tsx
@@ -1,4 +1,4 @@
-import { $ } from "@builder.io/qwik"
+import { $, type Signal } from "@builder.io/qwik"
 import { component$, useSignal, useTask$ } from "@builder.io/qwik"
 import { routeLoader$ } from "@builder.io/qwik-city"
 import { type BaseReponse } from "~/Commons/Base/BaseResponse"
@@ -8,51 +8,91 @@ import type {
 } from "~/Interface/Response/ILodgingReponseDto"
 
 export const useHotelDetail = routeLoader$(async (requestEvent) => {
-    // const res = await fetch(
-    //     `http://localhost:5278/api/Lodging/lodgings?${requestEvent.params.filter}`
-    // )
-    // const data: BaseReponse<ILodgingReponseDto<Items[]>> = await res.json()
     return requestEvent.params.filter
 })
 
 type PropLodgingPage = {
     ruta?: string
     nextPage: number
+    prevPage: number
+    todo: Signal<BaseReponse<ILodgingReponseDto<Items[]>> | undefined>
 }
 
-export const Test = component$<PropLodgingPage>(({ nextPage, ruta }) => {
-    const todo = useSignal<BaseReponse<ILodgingReponseDto<Items[]>>>()
+export const Test = component$<PropLodgingPage>(
+    ({ prevPage, nextPage, ruta, todo }) => {
+        const starRating = (start: number) => {
+            const stars = []
+            for (let i = 0; i < start; i++) {
+                stars.push("x")
+            }
+            return <span>{stars.join("")}</span>
+        }
+        const formater = (date: string) => {
+            const fecha = new Date(date)
+            return `${fecha.getFullYear()}/${
+                fecha.getMonth() + 1
+            }/${fecha.getDay()}`
+        }
+        // TODO: Para Cambiar de pagina
 
-    // TODO: Para Cambiar de pagina
-    const handlePageNext = $(async (page: number) => {
-        const res = await fetch(
-            `http://localhost:5278/api/Lodging/lodgings?NumPage=${page}&${ruta}`
-        )
-        const data: BaseReponse<ILodgingReponseDto<Items[]>> = await res.json()
-        console.log(data.data.items.length)
-        todo.value = data
-    })
+        const handlePage = $(async (direction: "next" | "prev") => {
+            const newPage = direction === "next" ? nextPage + 1 : prevPage - 1 // <-- determinar la nueva página según la dirección
+            const res = await fetch(
+                `http://localhost:5278/api/Lodging/lodgings?NumPage=${newPage}&${ruta}`
+            )
+            const data: BaseReponse<ILodgingReponseDto<Items[]>> =
+                await res.json()
+            todo.value = data
+        })
 
-    useTask$(async () => {
-        const res = await fetch(
-            `http://localhost:5278/api/Lodging/lodgings?NumPage=${nextPage}&${ruta}`
+        return (
+            <div>
+                <button onClick$={() => handlePage("next")}>
+                    Siguiente Pagina
+                </button>
+                <br />
+                <button onClick$={() => handlePage("prev")}>
+                    Anterior Pagina
+                </button>
+                <h1>{todo.value?.data.items.length}</h1>
+                {todo.value?.data.items.map((item) => (
+                    <div key={item.id} class="flex justify-center">
+                        <div class="m-3 h-[333px] border border-black rounded-xl grid grid-cols-text">
+                            <div class="bg-slate-400 text-center m-2"></div>
+                            <div class="flex flex-col p-2">
+                                <h1 class="text-3xl font-semibold">
+                                    {item.locality}
+                                </h1>
+                                <span>{item.lodgingType}</span>
+                                <span>{item.description}</span>
+                                {starRating(item.rating)}
+                            </div>
+                            <div class="p-2">
+                                <span class="block">
+                                    Fecha de incio: {formater(item.dateStart)}
+                                </span>
+                                <span class="block">
+                                    Fecha de fin: {formater(item.dateEnd)}
+                                </span>
+                                <h1 class="text-3xl">Precio: ${item.price}</h1>
+                                <p>No incluye impto.PAIS ni Percepciones.</p>
+                                <p>{item.description}</p>
+                                <p class="text-[#4300d2]">
+                                    ¿Que incluye es precio?
+                                </p>
+                                <div class="text-center">
+                                    <button class="bg-[#4300d2] text-white">
+                                        Ver Detalle
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         )
-        const data: BaseReponse<ILodgingReponseDto<Items[]>> = await res.json()
-        todo.value = data
-    })
-    return (
-        <div>
-            {todo.value?.data.items.map((item) => (
-                <div key={item.id}>
-                    <h1>{item.price}</h1>
-                </div>
-            ))}
-            <button onClick$={() => handlePageNext(nextPage)}>
-                Siguiente Pagina
-            </button>
-        </div>
-    )
-})
+    }
+)
 
 export default component$(() => {
     const signal = useHotelDetail()
@@ -60,65 +100,24 @@ export default component$(() => {
     const todo = useSignal<BaseReponse<ILodgingReponseDto<Items[]>>>()
 
     useTask$(async () => {
+        console.log(signal.value)
         const res = await fetch(
-            `http://localhost:5278/api/Lodging/lodgings?NumPage=${pagination.value}&${signal.value}`
+            `http://localhost:5278/api/Lodging/lodgings?${signal.value}`
         )
         const data: BaseReponse<ILodgingReponseDto<Items[]>> = await res.json()
+        console.log(data.data.items.length)
         todo.value = data
     })
 
-    const starRating = (start: number) => {
-        const stars = []
-        for (let i = 0; i < start; i++) {
-            stars.push("x")
-        }
-        return <span>{stars.join("")}</span>
-    }
-    const formater = (date: string) => {
-        const fecha = new Date(date)
-        return `${fecha.getFullYear()}/${
-            fecha.getMonth() + 1
-        }/${fecha.getDay()}`
-    }
     return (
         <div>
-            <h1>{todo.value?.data.items.length}</h1>
-            {todo.value?.data.items.map((item) => (
-                <div key={item.id} class="flex justify-center">
-                    <div class="m-3 h-[333px] border border-black rounded-xl grid grid-cols-text">
-                        <div class="bg-slate-400 text-center m-2"></div>
-                        <div class="flex flex-col p-2">
-                            <h1 class="text-3xl font-semibold">
-                                {item.locality}
-                            </h1>
-                            <span>{item.lodgingType}</span>
-                            <span>{item.description}</span>
-                            {starRating(item.rating)}
-                        </div>
-                        <div class="p-2">
-                            <span class="block">
-                                Fecha de incio: {formater(item.dateStart)}
-                            </span>
-                            <span class="block">
-                                Fecha de fin: {formater(item.dateEnd)}
-                            </span>
-                            <h1 class="text-3xl">Precio: ${item.price}</h1>
-                            <p>No incluye impto.PAIS ni Percepciones.</p>
-                            <p>{item.description}</p>
-                            <p class="text-[#4300d2]">
-                                ¿Que incluye es precio?
-                            </p>
-                            <div class="text-center">
-                                <button class="bg-[#4300d2] text-white">
-                                    Ver Detalle
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
             <div>Total de paginas: {todo.value?.data.totalPages}</div>
-            <Test ruta={signal.value} nextPage={pagination.value++} />
+            <Test
+                ruta={signal.value}
+                nextPage={pagination.value++}
+                prevPage={pagination.value--}
+                todo={todo}
+            />
         </div>
     )
 })
