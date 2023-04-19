00233392f1cb84d9ad788022bd348e1fe1dfb849
CoreDX1 <chrismaquis@gmail.com>
2023-04-19 16:36:49 -0300

    Refactoriznado

--- src/POS.Api/ClientApp/src/routes/(common)/alojamientos/[filter]/index.tsx
+++ src/POS.Api/ClientApp/src/routes/(common)/alojamientos/[filter]/index.tsx
@@ -8,37 +8,69 @@ import type {
 
 export const useHotelDetail = routeLoader$(async (requestEvent) => {
     const res = await fetch(
-        `http://localhost:5278/api/Lodging/filter/${requestEvent.params.filter}
-`,
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }
+        `http://localhost:5278/api/Lodging/lodgings?${requestEvent.params.filter}`
     )
-    const data = await res.json()
-    return data as BaseReponse<ILodgingReponseDto<Items[]>>
+    const data: BaseReponse<ILodgingReponseDto<Items[]>> = await res.json()
+    return data
 })
 
 // numPage=1&numRecordPage=10&numFilters=1&textLodgingType=hotel&textFilter=Madrid&startData=2022-01-01&endData=2022-01-10
 export default component$(() => {
     const signal = useHotelDetail()
+    const starRating = (start: number) => {
+        const stars = []
+        for (let i = 0; i < start; i++) {
+            stars.push("x")
+        }
+        return <span>{stars.join("")}</span>
+    }
+    const formater = (date: string) => {
+        const fecha = new Date(date)
+        return `${fecha.getFullYear()}/${
+            fecha.getMonth() + 1
+        }/${fecha.getDay()}`
+    }
     return (
         <div>
             <h1>{signal.value.data.items.length}</h1>
             {signal.value.data.items.map((item) => (
-                <div key={item.id} class="bg-gray-400 flex justify-center">
-                    <div class="p-7">
-                        <h1>{item.locality}</h1>
-                        <span>{item.lodgingType}</span>
-                        <br />
-                        <span>{item.dateStart}</span>
-                        <span>{item.dateEnd}</span>
-                        <p>{item.description}</p>
+                <div key={item.id} class="flex justify-center">
+                    <div class="m-3 h-[333px] border border-black rounded-xl grid grid-cols-text">
+                        <div class="bg-slate-400 text-center m-2"></div>
+                        <div class="flex flex-col p-2">
+                            <h1 class="text-3xl font-semibold">
+                                {item.locality}
+                            </h1>
+                            <span>{item.lodgingType}</span>
+                            <span>{item.description}</span>
+                            {starRating(item.rating)}
+                        </div>
+                        <div class="p-2">
+                            <span class="block">
+                                Fecha de incio: {formater(item.dateStart)}
+                            </span>
+                            <span class="block">
+                                Fecha de fin: {formater(item.dateEnd)}
+                            </span>
+                            <h1 class="text-3xl">Precio: ${item.price}</h1>
+                            <p>No incluye impto.PAIS ni Percepciones.</p>
+                            <p>{item.description}</p>
+                            <p class="text-[#4300d2]">
+                                ¿Que incluye es precio?
+                            </p>
+                            <div class="text-center">
+                                <button class="bg-[#4300d2] text-white">
+                                    Ver Detalle
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             ))}
+            <div>
+                Paginate{" "}
+                {signal.value.data.items.map((item) => item.description)}
+            </div>
         </div>
     )
 })
