79f1c829b82f6d10f090ed93fe8103600bb86bfb
CoreDX1 <chrismaquis@gmail.com>
2023-04-27 13:38:47 -0300

    Eliminado el using

--- src/POS.Application/Extensions/InjectionExtensions.cs
+++ src/POS.Application/Extensions/InjectionExtensions.cs
@@ -2,7 +2,6 @@ using System.Reflection;
 using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using POS.Application.Commons.Base;
 using POS.Application.DTO.Request.User;
 using POS.Application.ErrorValidation.UserError;
 using POS.Application.Interface;
